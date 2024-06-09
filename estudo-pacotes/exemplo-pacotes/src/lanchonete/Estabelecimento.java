263ae9bb0dcd2124fafae07beaf70317a617b755
Rfrodrigues37 <r.f.rodriguess@gmail.com.email>
2024-06-09 20:19:40 -0300

    Fundamentos da POO - Visibilidade dos recursos

--- estudo-pacotes/exemplo-pacotes/src/lanchonete/Estabelecimento.java
+++ estudo-pacotes/exemplo-pacotes/src/lanchonete/Estabelecimento.java
@@ -1,52 +1,28 @@
 package lanchonete;
 
+import lanchonete.area.cliente.Cliente;
+import lanchonete.atendimento.Atendente;
+import lanchonete.atendimento.cozinha.Almoxarife;
+import lanchonete.atendimento.cozinha.Cozinheiro;
+
 public class Estabelecimento {
 
     public static void main(String[] args) {
         Cozinheiro cozinheiro = new Cozinheiro();
-        //açoes que não precisam estarem disponiveis pata toda aplicação
-        cozinheiro.lavarIngrtedientes();
-        cozinheiro.baterVitaminaLiquidificador();
-        cozinheiro.selecionarIngrediantesVitamina();
-        cozinheiro.prepararLanche();
-        cozinheiro.prepararVitamina();
-        cozinheiro.prepararVitamina();
-
+        
         //ações que o estabeleescimento precisa ter ciencia
         cozinheiro.adicionarSucoNoBalcao();
         cozinheiro.adicionarLancheNoBalcao();
-        cozinheiro.adicionarComboNoBalcao();
-
-        Almoxarife almoxarife = new Almoxarife();
-        //ações que não precisam estarem disponiveis para toda a aplicação
-        almoxarife.controlarEntrada();
-        almoxarife.controlarSaida();
-        //ação que somente o seu pacote cozinha precisa conhecer (default)
-        almoxarife.entregarIngredientes();
-        almoxarife.trocarGas();
+        cozinheiro.adicionarComboNoBalcao();     
 
         Atendente atendente = new Atendente();
-        atendente.pegarLancheCozinha();
-        atendente.receberPagamento();
         atendente.servindoMesa();
-        //açao que somente o seu pacote cozinha precisa conhecer (default)
-        atendente.trocarGas();
-
+        atendente.receberPagamento();
+        
+        
         Cliente cliente = new Cliente ();
         cliente.escolherLanche();
         cliente.fazerPedido();
         cliente.pagarConta();
-
-        //não deveria, mas o estabelescimento
-        //ainda não definiu normas de atendimento
-        cliente.pegarPedidoBalcao();
-
-        //esta ação é muito sigilosa, que tal ser privada
-        cliente.consultarSaldoAplicativo();
-
-        //ja pensou o sclientes ouvindo que o gás acabou
-        cozinheiro.pedirParaTrocarGas(atendente);
-        cozinheiro.pedirParaTrocarGas(almoxarife);
-    }
-    
+    }   
 }
--- estudo-pacotes/exemplo-pacotes/src/lanchonete/atendimento/cozinha/Cozinheiro.java
+++ estudo-pacotes/exemplo-pacotes/src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -1,49 +1,50 @@
-package lanchonete;
+package lanchonete.atendimento.cozinha;
+
+import lanchonete.atendimento.Atendente;
 
 public class Cozinheiro {
- 
+    //pode ser default
     public void adicionarLancheNoBalcao(){
         System.out.println("ADICIONANDO LANCHE NATUAL HAMBURGER NO BALCÃO.");
         }
+    //pode ser default
     public void adicionarSucoNoBalcao(){
         System.out.println("ADICIONANDO SUCO NO BALCÃO.");
         }
+    //pode ser default
     public void adicionarComboNoBalcao(){
         adicionarSucoNoBalcao();
         adicionarLancheNoBalcao();
         }
-    public void prepararLanche() {
+    private void prepararLanche() {
         System.out.println("PREPARANDO LANCHE TIPO HAMBURGER");
         }
-    public void prepararVitamina() {
+    private void prepararVitamina() {
         System.out.println("PREPARANDO SUCO");      
         }
-    public void prepararCombo(){
+    private void prepararCombo(){
         prepararLanche();
         prepararVitamina();
         }
-    public void selecionarIngrediantesLanche() {
+    private void selecionarIngrediantesLanche() {
         System.out.println("SELECIONADO O PÃO, SALADA, OVO E CARNE");
         }
-    public void selecionarIngrediantesVitamina() {
+    private void selecionarIngrediantesVitamina() {
         System.out.println("SELECIONADO FRUTA, LEITE E SUCO");
         }
-    public void lavarIngrtedientes() {
+    private void lavarIngredientes() {
         System.out.println("LAVANDO INGREDIENTES");
         }
-    public void baterVitaminaLiquidificador() {
+    private void baterVitaminaLiquidificador() {
         System.out.println("BATENDO VITAMINA LIQUIDIFICADOR");
         }
     public void fritarIngredientesLancha() {
         System.out.println("FRITANDO A CARNE E OVO PARA O HAMBURGER");
         }
-    public void pedirParaTrocarGas(Atendente meuAmigo) {
-        meuAmigo.trocarGas();        
-        }
-    public void pedirParaTrocarGas(Almoxarife meuAmigo) {
+    private void pedirParaTrocarGas(Almoxarife meuAmigo) {
         meuAmigo.trocarGas();
         }   
-    public void pedirIngredientes(Almoxarife almoxarife) {
+    private void pedirIngredientes(Almoxarife almoxarife) {
         almoxarife.entregarIngredientes();
         }
 }

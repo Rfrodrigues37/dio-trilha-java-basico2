597c43f433c6508db87d19d51b93143b92531e9e
Rfrodrigues37 <r.f.rodriguess@gmail.com.email>
2024-06-09 22:48:12 -0300

    Tentativa de construção de um programa para calendario de turno

--- calendario/calendario/src/Mapa.java
+++ calendario/calendario/src/Mapa.java
@@ -1,8 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
 
 public class Mapa {
     
     public static void main(String[] args) {
-        //criação dos dados do turno
+        //Rotina que cria os turnos dados do turno inserindo os dias trabalhados e folga
         Turnos N1 = new Turnos(); // N1 = 08 as 16
         N1.dias = 4;
         N1.folga = 1;
@@ -14,8 +20,37 @@ public class Mapa {
         Turnos N3 = new Turnos(); // N1 = 00 as 08
         N3.dias = 4;
         N3.folga = 2;
+        
+             
+
+        int diferencaDiasInt = caputurarData();
+        System.out.println("Diferença em dias: " + diferencaDiasInt);
 
     }
+            
+          public static int caputurarData() {
+            LocalDate data1 = LocalDate.of(2024, 1, 2);
+            Scanner scanner = new Scanner(System.in);
+            LocalDate data2 = null;
+            boolean entradaValida = false;
+            
+            while(!entradaValida) {
+                 System.out.println("Digite a data desejada para verificar a folga: ");
+                 String data2Str = scanner.nextLine();       
+
+                    try {
+                        data2 = LocalDate.parse(data2Str, DateTimeFormatter.ISO_DATE);
+                        entradaValida = true;
+                    } catch (DateTimeParseException e) {
+
+                         System.out.println("Formato de data inválido. Por favoe tente novamente");
+                    }
+                }
+                long diferencaDias = ChronoUnit.DAYS.between(data1, data2);
+                int diferencaDiasInt = (int) diferencaDias;
+                return (int) diferencaDiasInt;
+
+           }
     
 
 }
